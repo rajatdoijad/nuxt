58fc614419db76f428b71c9eb117bb342a66369f
Rajat <rajatdoijad67@gmail.com>
2022-07-27 18:12:33 +0530

    nuxt project

---
+++ Pages/index.vue
@@ -0,0 +1,38 @@
+<!--<template>
+    
+ 
+    <div>
+        <h3 class="flex justify-center bg-green-400 " >   hello welcome to pune</h3>
+       
+        <button class=" bg-red-200 m-15 p-10 font-bold">click me</button>
+    </div>
+</template>
+-->
+<template>
+ <h1 class="flex justify-center bg-green-300 " >   hello welcome to pune</h1>
+       
+<button class=" bg-red-200 m-15 p-10 flex-bold ">click me</button>
+  <div class="content-center">
+    <h1 class="flex justify-center">Hello !! </h1>
+    <h2 class="flex justify-center">Welcome to Nuxt it is framework of Vue js</h2>
+  <div class=" justify-center">
+    <div >
+     <h4 class="flex justify-center" >1)Choose your FrameWork</h4>
+     <div class="flex justify-center">
+        <button class="rounded-full bg-blue-100 text-2xl px-3">Vue js</button>
+        <button class="rounded-full bg-blue-100 text-2xl px-3"> Nuxt js</button>
+        <button class="rounded-full bg-blue-100 text-2xl px-3">React</button>
+        </div>
+    </div>
+    <div>
+     <h4 class="flex justify-center" >2)Choose your Intrest Area</h4>
+     <div class="flex justify-center" >
+        <button class="rounded-full bg-blue-100 text-2xl px-3">Front End</button>
+        <button class="rounded-full bg-blue-100 text-2xl px-3">Back End</button>
+        <button class="rounded-full bg-blue-100 text-2xl px-3">Testing</button>
+        </div>
+    </div>
+    <div class="flex justify-center"></div>
+  </div>
+  </div>
+</template>
