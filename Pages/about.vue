b9f84afdef9770687a7518ddb34a69f942bfda16
Rajat <rajatdoijad67@gmail.com>
2022-07-28 18:20:00 +0530

    second commit

---
+++ Pages/about.vue
@@ -0,0 +1,7 @@
+
+
+<template>
+  <h1 class="text-3xl font-bold underline bg-grey-600 flex justify-center">
+    Hello world!
+  </h1>
+</template>
--- Pages/index.vue
+++ Pages/index.vue
@@ -1,23 +1,21 @@
 <!--<template>
-    
- 
-    <div>
+ <div>
         <h3 class="flex justify-center bg-green-400 " >   hello welcome to pune</h3>
        
         <button class=" bg-red-200 m-15 p-10 font-bold">click me</button>
     </div>
 </template>
 -->
-<template>
- <h1 class="flex justify-center bg-green-300 " >   hello welcome to pune</h1>
+<!----<template>
+ <h1 class="flex justify-center bg-pink-500 height" >welcome</h1>
        
-<button class=" bg-red-200 m-15 p-10 flex-bold ">click me</button>
+<button class=" bg-red-800 ml-10 p-10 flex-bold ">click me</button>
   <div class="content-center">
     <h1 class="flex justify-center">Hello !! </h1>
     <h2 class="flex justify-center">Welcome to Nuxt it is framework of Vue js</h2>
   <div class=" justify-center">
     <div >
-     <h4 class="flex justify-center" >1)Choose your FrameWork</h4>
+     <h4 class="flex justify-center" >1 Choose your FrameWork</h4>
      <div class="flex justify-center">
         <button class="rounded-full bg-blue-100 text-2xl px-3">Vue js</button>
         <button class="rounded-full bg-blue-100 text-2xl px-3"> Nuxt js</button>
@@ -35,4 +33,27 @@
     <div class="flex justify-center"></div>
   </div>
   </div>
+</template>-->
+
+<template>
+  <div>
+    <div>Counter:{{count}}</div>
+    <div>Plus One:{{plusOne}}</div>
+   <button class="w-32 rounded-lg bg-teal-500 text-white mx-1" @click="decrement">Decrement</button>
+   <button class="w-32 rounded-lg bg-teal-500 text-white mx-1 " @click="increment">Increment</button>
+  </div>
 </template>
+<script setup>
+//import {ref} from vue
+const count=ref(0)
+const plusOne=computed(()=>count.value + 1)
+function decrement(){
+    count.value--
+}
+function increment(){
+    count.value++
+}
+</script>
+<style>
+</style>
+
